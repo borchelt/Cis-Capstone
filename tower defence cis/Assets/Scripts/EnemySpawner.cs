b1a2863ae343b810fe3e72cc2ad13c38c1b2c17c
Greenflex <jsos30@icloud.com>
2023-02-13 14:46:44 -0800

    Added Enemy Spawner Prefab
    Still working on other spawner variants, but I wanted to put in the most basic spawner readily available. The spawner works fine, but an error seems to occur every time an enemy is not present. Perhaps the tower AI is trying to look for the next enemy, even though enemies are (before the enemy spawns) absent?

---
+++ tower defence cis/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // variables and objects set
+    public GameObject enemy;
+
+    // timer initiated for enemies
+    void Start()
+    {
+        StartCoroutine(SpawnTimerEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnTimerEnemy();
+    }
+
+    // to spawn an Enemy
+    private void spawnEnemy()
+    {
+        // Game object set for XY spawning coords
+        GameObject enemyOBJ;
+        // Enemy object is made in-game at the spawner XY coords
+        enemyOBJ = Instantiate(enemy, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+    }
+
+    // timer for Enemies to spawn
+    IEnumerator SpawnTimerEnemy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2.0f);
+            spawnEnemy();
+        }
+    }
+}
