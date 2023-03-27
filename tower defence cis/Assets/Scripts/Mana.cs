835ceb6c2973f09af9056559c20562452f19b479
YggdrasilsArtisan <62152298+YggdrasilsArtisan@users.noreply.github.com>
2023-03-27 10:17:52 -0400

    Updated mana script

--- tower defence cis/Assets/Scripts/Mana.cs
+++ tower defence cis/Assets/Scripts/Mana.cs
@@ -2,24 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//This script is for mana generation
+//This script is for mana generation and usage
 
 public class Mana : MonoBehaviour
 {
-    private int currentManaAmount;
     private int towerManaAmount;
-    private int totalManaTowerLevel;
+    private int currentManaAmount;
+    private int boostedTowerManaAmount;
     private int numberOfManaTowers;
+    private int towerLevel;
+    private int manaCost;
 
     private float manaGenerationRate;
+    private float boostedManaGenerationRate;
 
     bool manaTowersExist;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    bool useSpell;
+    bool buildTower;
+    bool upgradeTower;
 
     // Update is called once per frame
     void Update()
@@ -34,8 +34,6 @@ public class Mana : MonoBehaviour
         {
             StopCoroutine("GenerateMana");
         }
-
-        Debug.Log("Mana: " + currentManaAmount);
     }
 
     private void CheckForManaTowers()
@@ -44,23 +42,119 @@ public class Mana : MonoBehaviour
         {
             manaTowersExist = true;
         }
-            
+
+        numberOfManaTowers = GameObject.FindGameObjectsWithTag("ManaTower").Length;
     }
 
     private int DetermineTowerManaAmount()
     {
-        towerManaAmount = totalManaTowerLevel * numberOfManaTowers;
+        for (int i = 0; i < numberOfManaTowers; i++)
+        {
+            if (towerLevel == 1)
+            {
+                boostedTowerManaAmount += 10;
+            }
+            else if (towerLevel == 2)
+            {
+                boostedTowerManaAmount += 20;
+            }
+            else if (towerLevel == 3)
+            {
+                boostedTowerManaAmount += 40;
+            }
+            else if (towerLevel == 4)
+            {
+                boostedTowerManaAmount += 80;
+            }
+            else if (towerLevel == 5)
+            {
+                boostedTowerManaAmount += 160;
+            }
+
+            towerManaAmount += boostedTowerManaAmount;
+        }
+
+        towerManaAmount = 70 + boostedTowerManaAmount;
         return towerManaAmount;
     }
 
     private void DetermineManaGenerationRate()
     {
-        manaGenerationRate = DetermineTowerManaAmount() / 10;
+        for (int i = 0; i < numberOfManaTowers; i++)
+        {
+            if (towerLevel == 1)
+            {
+                boostedManaGenerationRate += 0.5f;
+            }
+            else if (towerLevel == 2)
+            {
+                boostedManaGenerationRate += 1.0f;
+            }
+            else if (towerLevel == 3)
+            {
+                boostedManaGenerationRate += 2.0f;
+            }
+            else if (towerLevel == 4)
+            {
+                boostedManaGenerationRate += 4.0f;
+            }
+            else if (towerLevel == 5)
+            {
+                boostedManaGenerationRate += 8.0f;
+            }
+
+            boostedManaGenerationRate += boostedManaGenerationRate;
+        }
+        
+        manaGenerationRate = 100 / (1 + boostedManaGenerationRate);
+    }
+
+    private int DetermineManaCostAmount()
+    {
+        if (useSpell)
+        {
+            manaCost = 3000;
+        }
+        else if (buildTower)
+        {
+            manaCost = 1000;
+        }
+        else if (upgradeTower)
+        {
+            if (towerLevel == 1)
+            {
+                manaCost = 500;
+            }
+            else if (towerLevel == 2)
+            {
+                manaCost = 1000;
+            }
+            else if (towerLevel == 3)
+            {
+                manaCost = 3000;
+            }
+            else if (towerLevel == 4)
+            {
+                manaCost = 12000;
+            }
+            else if (towerLevel == 5)
+            {
+                manaCost = 60000;
+            }
+        }
+
+        return manaCost;
     }
 
     private void AddMana()
     {
         currentManaAmount += DetermineTowerManaAmount();
+    }
+
+    private void LoseMana()
+    {
+        manaCost = DetermineManaCostAmount();
+        currentManaAmount -= manaCost;
     }
  
     IEnumerator GenerateMana()
