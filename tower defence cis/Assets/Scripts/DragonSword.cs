3e244549201893b9bbf83f3470b14fb681427cf7
YggdrasilsArtisan <62152298+YggdrasilsArtisan@users.noreply.github.com>
2023-04-04 11:58:18 -0400

    Added player sprite and created script for it
    Also, there is an error in the last line of the previous commit description.
    Only the Mana script interacts with the UI. Mana Tower does not.

---
+++ tower defence cis/Assets/Scripts/DragonSword.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is for ending the game when the Dragon Sword takes too much damage
+
+public class DragonSword : MonoBehaviour
+{
+    public int dragonSwordHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
