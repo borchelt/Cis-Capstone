3cf4c2449f528aacd0cf89b2fd84e708e4d9c362
borchelt <wolfgangborchelt@gmail.com>
2023-04-10 16:01:20 -0400

    wave update
    revamped how waves of enemies spawn
    
    - the spawn manager now has a list of items to spawn and cooldowns for all of them
    
    - each cooldown ticks down when it's the next wave to be spawned
    
    - on zero, it spawns the wave and moves to the next one

--- tower defence cis/Assets/Scripts/WaveManager.cs
+++ tower defence cis/Assets/Scripts/WaveManager.cs
@@ -13,45 +13,42 @@ public class WaveManager : MonoBehaviour
         // - each enemy contributes score, if the total score is under the score limit the next wave is spawned 
         public GameObject enemy;
         public int count;
-        public int scoreLimit;
-        public waveDef subWave;
+        public float timeWait;
     }
 
     //stuff for getting the score and spawning the waves
-    GameObject[] targetList;
     public List<waveDef> levelLayout;
-    public int currentScore;
     public int waveIndex = 0;
+    bool lastWave = false;
 
     // Update is called once per frame
     void Update()
     {
-        checkWaveScore();
+        levelLayout[waveIndex].timeWait -= Time.deltaTime;
         waveManager();
     }
 
     //finds the current score of the scene
-    void checkWaveScore()
-    {
-        currentScore = 0;
-        targetList = GameObject.FindGameObjectsWithTag("enemy");
+    //void checkWaveScore()
+    //{
+     //   currentScore = 0;
+     //   targetList = GameObject.FindGameObjectsWithTag("enemy");
+     //
+     //   foreach(GameObject enemy in targetList)
+     //   {
+     //      EnemyMovement scorefinder = enemy.GetComponent<EnemyMovement>();
+     //       if(scorefinder!=null)
+     //           currentScore += scorefinder.score;
 
-        foreach(GameObject enemy in targetList)
-        {
-            EnemyMovement scorefinder = enemy.GetComponent<EnemyMovement>();
-            if(scorefinder!=null)
-                currentScore += scorefinder.score;
-
-        }
+     //   }
         
-    }
+    //}
 
     //if the current score is lower than the score limit, spawn the next wave
     void waveManager()
     {
-        Debug.Log("currentScore: " + currentScore);
-        Debug.Log("threshhold: " + levelLayout[waveIndex].scoreLimit);
-        if (currentScore <= levelLayout[waveIndex].scoreLimit)
+        Debug.Log("threshhold: " + levelLayout[waveIndex].timeWait);
+        if (levelLayout[waveIndex].timeWait <= 0 && !lastWave)
         {
             waveDef wave = levelLayout[waveIndex];
             spawnWave(wave);
@@ -64,14 +61,20 @@ public class WaveManager : MonoBehaviour
         for(int i = 1; i <= wave.count; i++)
         {
             GameObject enemyOBJ;
-            Vector2 randomVector = new Vector2(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+            Vector2 randomVector = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f));
             // Enemy object is made in-game at the spawner XY coords + the random vector
             enemyOBJ = Instantiate(wave.enemy, new Vector2(transform.position.x + randomVector.x, transform.position.y + randomVector.y), Quaternion.identity);
         }
 
         //once the wave is spawned, move to the next wave
-        if(waveIndex < levelLayout.Count-1)
+        if (waveIndex < levelLayout.Count - 1)
             waveIndex++;
+        else if (waveIndex >= levelLayout.Count - 1)
+        {
+            lastWave = true;
+            return;
+        }
+            
     }
 
 
